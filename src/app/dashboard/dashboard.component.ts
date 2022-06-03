d01ea35cf13d28df0ebc314237fb05e974523137
aswin <aswinpalayad@ymail.com>
2022-06-03 13:45:15 +0530

    form validation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -16,15 +17,30 @@ export class DashboardComponent implements OnInit {
   pswd1=""
   amount1=""
 
-  constructor(private ds:DataService) { }
+  //deposit form group
+  depositForm=this.fb.group({
+    acno:['',[Validators.required,Validators.pattern('[0-9]*')]],
+    pswd:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+    amount:['',[Validators.required,Validators.pattern('[0-9]*')]]
+  })
+
+  //withdraw form group
+
+  withdrawForm=this.fb.group({
+    acno1:['',[Validators.required,Validators.pattern('[0-9]*')]],
+    pswd1:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+    amount1:['',[Validators.required,Validators.pattern('[0-9]*')]]
+  })
+
+  constructor(private ds:DataService,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   deposit(){
-    var acno=this.acno
-    var pswd=this.pswd
-    var amount=this.amount
+    var acno=this.depositForm.value.acno
+    var pswd=this.depositForm.value.pswd
+    var amount=this.depositForm.value.amount
 
     const result=this.ds.deposit(acno,pswd,amount)
 
@@ -35,9 +51,9 @@ export class DashboardComponent implements OnInit {
   }
 
   withdraw(){
-    var acno=this.acno1
-    var pswd=this.pswd1
-    var amount=this.amount1
+    var acno=this.withdrawForm.value.acno1
+    var pswd=this.withdrawForm.value.pswd1
+    var amount=this.withdrawForm.value.amount1
 
     const result=this.ds.withdraw(acno,pswd,amount)
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -14,10 +15,15 @@ export class LoginComponent implements OnInit {
   acno=""
   pswd=""
 
+  //form group
+  loginForm=this.fb.group({
+    acno:['',[Validators.required,Validators.pattern('[0-9]*')]],
+    pswd:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]],
+  })
   
 
 
-  constructor(private router:Router,private ds:DataService) { }
+  constructor(private router:Router,private ds:DataService,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
@@ -36,8 +42,8 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    var acno=this.acno
-    var pswd=this.pswd
+    var acno=this.loginForm.value.acno
+    var pswd=this.loginForm.value.pswd
 
     const result = this.ds.login(acno,pswd)
 
